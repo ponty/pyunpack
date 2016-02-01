cb50c956d448f49d41d3b94e16bbfc2afb89c174
ponty <ponty@home>
2016-02-01 12:29:27 +0100

    test

--- tests/test.py
+++ tests/test.py
@@ -1,57 +1,70 @@
-from easyprocess import Proc
-from nose.tools import ok_, eq_
+from easyprocess import EasyProcess
+from nose.tools import ok_, eq_, assert_raises
+from path import Path
 from pyunpack import Archive, PatoolError, cli
-from unittest import TestCase
-import os.path
 import sys
 import tempfile
 
 
 def ok_file(d, f):
-    full = os.path.join(d, 'x.txt')
-    ok_(os.path.exists(full), full)
-    eq_(open(full).read(), '123')
+    full = d / 'x.txt'
+    ok_(full.exists(), full)
+    eq_(full.text(), '123')
 
 
 def tmpdir():
     d = tempfile.mkdtemp(prefix='pyunpack_test_')
-    return d
+    return Path(d)
 
 
-class Test(TestCase):
-    def test(self):
-        self.assertRaises(
-            ValueError, lambda: Archive('blabla').extractall('/tmp'))
-        self.assertRaises(
-            PatoolError, lambda: Archive(__file__).extractall('/tmp'))
+def test():
+    assert_raises(ValueError, lambda: Archive('blabla').extractall('/tmp'))
+    assert_raises(PatoolError, lambda: Archive(__file__).extractall('/tmp'))
 
-    def test2(self):
-        d = tempfile.mkdtemp()
-        x_txt = os.path.join(d, 'x.txt')
-        open(x_txt, 'w').write('123')
-        x_zip = os.path.join(d, 'x.zip')
-        Proc(['zip', '--no-dir-entries', x_zip, 'x.txt'], cwd=d).call()
 
-        self.assertRaises(
-            ValueError, lambda: Archive(x_zip).extractall('blabla'))
+def create_zip():
+    d = tmpdir()
+    x_txt = d / 'x.txt'
+    x_txt.write_text('123')
+    x_zip = d / 'x.zip'
+    EasyProcess(['zip', '--no-dir-entries', x_zip, 'x.txt'], cwd=d).call()
+    return x_zip
 
-        d = tempfile.mkdtemp()
-        Archive(x_zip, backend='patool').extractall(d)
+
+def test2():
+    x_zip = create_zip()
+
+    assert_raises(ValueError, lambda: Archive(x_zip).extractall('blabla'))
+
+    d = tmpdir()
+    Archive(x_zip, backend='patool').extractall(d)
+    ok_file(d, 'x.txt')
+
+    d = tmpdir()
+    Archive(x_zip).extractall(d)
+    ok_file(d, 'x.txt')
+
+    d = tmpdir()
+    Archive(x_zip, backend='auto').extractall(d)
+    ok_file(d, 'x.txt')
+
+    if sys.version_info >= (2, 6):
+        d = tmpdir()
+        Archive(x_zip, backend='zipfile').extractall(d)
         ok_file(d, 'x.txt')
 
-        d = tempfile.mkdtemp()
-        Archive(x_zip).extractall(d)
-        ok_file(d, 'x.txt')
+    d = tmpdir()
+    cli.extractall(x_zip, d)
+    ok_file(d, 'x.txt')
 
-        d = tempfile.mkdtemp()
-        Archive(x_zip, backend='auto').extractall(d)
-        ok_file(d, 'x.txt')
 
-        if sys.version_info >= (2, 6):
-            d = tempfile.mkdtemp()
-            Archive(x_zip, backend='zipfile').extractall(d)
-            ok_file(d, 'x.txt')
+def test_subdir():
+    x_zip = create_zip()
 
-        d = tempfile.mkdtemp()
-        cli.extractall(x_zip, d)
-        ok_file(d, 'x.txt')
+    d = tmpdir() / 'subdir'
+    assert_raises(ValueError, lambda: Archive(
+        x_zip).extractall(d, auto_create_dir=False))
+
+    d = tmpdir() / 'subdir'
+    Archive(x_zip, backend='auto').extractall(d, auto_create_dir=True)
+    ok_file(d, 'x.txt')
