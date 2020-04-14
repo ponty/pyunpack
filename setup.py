2c9eb305156446c3c13e0681e2014082acb928f6
ponty <ponty@home>
2020-04-14 18:08:21 +0200

    setup.py: remove hack

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ from setuptools import setup
 import os.path
 import sys
 
-if os.environ.get('distutils_issue8876_workaround_enabled', False):
-    # sdist_hack: Remove reference to os.link to disable using hardlinks when
-    #             building setup.py's sdist target.  This is done because
-    #             VirtualBox VMs shared filesystems don't support hardlinks.
-    del os.link
-
 NAME = 'pyunpack'
 URL = 'https://github.com/ponty/pyunpack'
 DESCRIPTION = 'unpack archive files'
