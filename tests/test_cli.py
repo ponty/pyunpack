14679cb59b0caaba77215b037a9ed2d22c3c3f3b
ponty <ponty@home>
2020-05-02 15:11:02 +0200

    test cli

---
+++ tests/test_cli.py
@@ -0,0 +1,27 @@
+import sys
+from easyprocess import EasyProcess
+
+help = """
+usage: cli.py [-h] [-b BACKEND] [-a] [--debug] filename directory
+
+positional arguments:
+  filename              path to archive file
+  directory             directory to extract to
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -b BACKEND, --backend BACKEND
+                        auto, patool or zipfile
+  -a, --auto-create-dir
+                        auto create directory
+  --debug               set logging level to DEBUG
+""".strip()
+python = sys.executable
+
+
+def test_help():
+    cmd = [python, "-m", "pyunpack.cli", "--help"]
+    h = EasyProcess(cmd).call().stdout.strip()
+    print(help)
+    print(h)
+    assert h == help
