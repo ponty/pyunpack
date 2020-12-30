170fca013a13ce06ffbe59cb90111690a96e1164
ponty <ponty@home>
2020-12-30 12:55:52 +0100

    remove py27, py36

--- setup.py
+++ setup.py
@@ -21,21 +21,14 @@ PACKAGES = [
     #             NAME + '.examples',
 ]
 
-# extra = {}
-# if sys.version_info >= (3,):
-#     extra['use_2to3'] = True
-
 classifiers = [
     # Get more strings from
     # http://www.python.org/pypi?%3Aaction=list_classifiers
     "License :: OSI Approved :: BSD License",
     "Natural Language :: English",
     "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
