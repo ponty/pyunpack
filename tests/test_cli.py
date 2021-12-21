6e0c20ef7118fad51fb7d9c080f1a07ed292f392
ponty <ponty@home>
2021-12-21 20:29:23 +0100

    test: fix 3.10 test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ import sys
 
 from easyprocess import EasyProcess
 
-help = """
+help_txt = """
 usage: cli.py [-h] [-b BACKEND] [-a] [--debug] filename directory
 
 positional arguments:
@@ -17,6 +17,10 @@ optional arguments:
                         auto create directory
   --debug               set logging level to DEBUG
 """.strip()
+# text changed in py3.10
+if sys.version_info[0] > 3 or sys.version_info[1] >= 10:
+    help_txt = help_txt.replace("optional arguments", "options")
+
 python = sys.executable
 
 
@@ -24,6 +28,6 @@ def test_help():
     cmd = [python, "-m", "pyunpack.cli", "--help"]
     h = EasyProcess(cmd).call().stdout.strip()
     h = h.replace("\r", "")  # for win
-    print(help)
+    print(help_txt)
     print(h)
-    assert h == help
+    assert h == help_txt
