afa6b1533b5797d1a9d06bbf024d00a5a33b9959
ponty <ponty@home>
2022-07-17 20:44:13 +0200

    test: GBK (character encoding) #15

---
+++ tests/test_zipchar.py
@@ -0,0 +1,43 @@
+import os
+import tempfile
+from os.path import dirname, join
+
+from pyunpack import Archive
+
+DIR = dirname(__file__)
+
+
+def tmpdir():
+    d = tempfile.mkdtemp(prefix="pyunpack_test_")
+    return d
+
+
+def ok_file(d):
+    full = join(d, "测试", "test", "test.txt")
+    assert os.path.exists(full)
+    full = join(d, "测试", "test", "测试.txt")
+    assert os.path.exists(full)
+
+
+def test_zipchar():
+    f = join(DIR, "testchar.zip")
+
+    d = tmpdir()
+    Archive(f, backend="patool").extractall(d)
+    ok_file(d)
+
+    d = tmpdir()
+    Archive(f, backend="zipfile").extractall(d)
+    ok_file(d)
+
+    # d = tmpdir()
+    # Archive(f).extractall(d)
+    # ok_file(d)
+
+    # d = tmpdir()
+    # Archive(f, backend="auto").extractall(d)
+    # ok_file(d)
+
+    # d = tmpdir()
+    # cli.extractall(f, d)
+    # ok_file(d)
